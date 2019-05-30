06414b147507b503e21a529d69256791841ed5f5
shektor <44544977+shektor@users.noreply.github.com>
2019-05-30 10:17:06 +0100

    Refactor Game functions to match named expectations
    Roll was previously calculating the score, updated to record game rolls. Score was previously returning the score, updated to iterate through roll array, calculate the score and return.

--- src/game.js
+++ src/game.js
@@ -1,12 +1,18 @@
 function Game() {
   this._score = 0;
+  this._rolls = [];
+  this._currentRoll = 0;
 }
 
 Game.prototype.roll = function(pins) {
-  this._score += pins;
+  this._rolls[this._currentRoll] = pins;
+  this._currentRoll++;
 };
 
 Game.prototype.score = function() {
+  for (var i = 0; i < this._rolls.length; i++) {
+    this._score += this._rolls[i];
+  }
   return this._score;
 };
 
